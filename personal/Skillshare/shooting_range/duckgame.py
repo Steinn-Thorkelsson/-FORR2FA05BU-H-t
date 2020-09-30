3e1dc4ebb141d3e8554605ee872791a2446afbb3
Steinn-Thorkelsson <steinntho20@tskoli.is>
2020-09-30 09:41:01 +0000

    shoot

---
+++ personal/Skillshare/shooting_range/duckgame.py
@@ -0,0 +1,46 @@
+import pygame
+import sys
+
+# General setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+
+wood_bg = pygame.image.load('Wood_BG.png')
+land_bg = pygame.image.load('Land_BG.png')
+water_bg = pygame.image.load('Water_BG.png')
+cloud1 = pygame.image.load('cloud1.png')
+cloud2 = pygame.image.load('cloud2.png')
+
+land_position_y = 560
+land_speed = 1
+water_position_y = 640
+water_speed = 1.5
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.blit(wood_bg, (0, 0))
+
+    # Animation
+    land_position_y += land_speed
+    if land_position_y <= 520 or land_position_y >= 600:
+        land_speed *= -1
+    screen.blit(land_bg, (0, land_position_y))
+
+    water_position_y += water_speed
+    if water_position_y <= 620 or water_position_y >= 680:
+        water_speed *= -1
+    screen.blit(water_bg, (0, water_position_y))
+
+    # Clouds
+    screen.blit(cloud1, (50, 80))
+    screen.blit(cloud2, (190, 20))
+    screen.blit(cloud1, (500, 80))
+    screen.blit(cloud2, (800, 20))
+    screen.blit(cloud1, (1100, 60))
+
+    pygame.display.update()
+    clock.tick(120)
