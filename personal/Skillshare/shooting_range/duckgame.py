5ec403e23d1eed56b296427319064ed904ae6349
Steinn-Thorkelsson <steinntho20@tskoli.is>
2020-10-02 13:29:57 +0000

    baba

--- personal/Skillshare/shooting_range/duckgame.py
+++ personal/Skillshare/shooting_range/duckgame.py
@@ -7,6 +7,7 @@ import sys
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+pygame.mouse.set_visible(False)
 
 wood_bg = pygame.image.load('Wood_BG.png')
 land_bg = pygame.image.load('Land_BG.png')
@@ -14,15 +15,18 @@ water_bg = pygame.image.load('Water_BG.png')
 cloud1 = pygame.image.load('cloud1.png')
 cloud2 = pygame.image.load('cloud2.png')
 crosshair = pygame.image.load('crosshair.png')
+crosshair_rect = crosshair.get_rect()
 duck_surface = pygame.image.load('duck.png')
 
-
+game_font = pygame.font.Font(None, 200)
+text_surface = game_font.render('You Won!', True, (255, 255, 255))
+text_rect = text_surface.get_rect(center=(640, 360))
 land_position_y = 560
 land_speed = 1
 water_position_y = 640
 water_speed = 1.5
-
 duck_list = []
+
 for duck in range(20):
     duck_position_x = random.randrange(50, 1200)
     duck_position_y = random.randrange(120, 600)
@@ -37,13 +41,16 @@ while True:
             crosshair_rect = crosshair.get_rect(center=event.pos)
         if event.type == pygame.MOUSEBUTTONDOWN:
             for index, duck_rect in enumerate(duck_list):
-                if crosshair_rect.colliderect(duck_rect):
+                if duck_rect.collidepoint(event.pos):  # .colliderect(duck_rect):
                     del duck_list[index]
 
     screen.blit(wood_bg, (0, 0))
 
     for duck_rect in duck_list:
         screen.blit(duck_surface, duck_rect)
+    if len(duck_list) == 0:
+        screen.blit(text_surface, text_rect)
+
     # Animation
     land_position_y += land_speed
     if land_position_y <= 520 or land_position_y >= 600:
@@ -56,13 +63,13 @@ while True:
     screen.blit(water_bg, (0, water_position_y))
 
     screen.blit(crosshair, crosshair_rect)
-
     # Clouds
     screen.blit(cloud1, (50, 80))
     screen.blit(cloud2, (190, 20))
     screen.blit(cloud1, (500, 80))
     screen.blit(cloud2, (800, 20))
     screen.blit(cloud1, (1100, 60))
+    # Text
 
     pygame.display.update()
     clock.tick(120)
