a088dbd8d61ffa63edf448ec25bf1b951061883b
Steinn-Thorkelsson <steinntho20@tskoli.is>
2020-10-06 00:51:51 +0000

    update

--- personal/Skillshare/Meteor Dodger/meteor_dodger.py
+++ personal/Skillshare/Meteor Dodger/meteor_dodger.py
@@ -20,13 +20,30 @@ class SpaceShip(pygame.sprite.Sprite):
             self.rect.left = 0
 
 
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, path, x_pos, y_pos, x_speed, y_speed):
+        super().__init__()
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(center=(x_pos, y_pos))
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+
+    def update(self):
+        self.rect.centerx += self.x_speed
+        self.rect.centery += self.y_speed
+
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
-spaceship = SpaceShip('Spaceship.png', 640, 500, 10)
+spaceship = SpaceShip('Spaceship.png', 640, 500, 7)
 spaceship_group = pygame.sprite.GroupSingle()
 spaceship_group.add(spaceship)
+
+meteor_1 = Meteor('Meteor1.png', 400, -100, 1, 10)
+meteor_group = pygame.sprite.GroupSingle()
+meteor_group.add(meteor_1)
 
 while True:
     for event in pygame.event.get():
@@ -37,6 +54,8 @@ while True:
     screen.fill((42, 45, 51))
 
     spaceship_group.draw(screen)
+    meteor_group.draw(screen)
     spaceship_group.update()
+    meteor_group.update()
     pygame.display.update()
     clock.tick(120)
