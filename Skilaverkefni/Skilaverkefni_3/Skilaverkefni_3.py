6f075a53225e3a62afd18b00cd6b9db320c26f0e
Steinn-Thorkelsson <steinntho20@tskoli.is>
2020-10-12 23:28:51 +0000

    Update Skilaverkefni_3.py

--- Skilaverkefni/Skilaverkefni_3/Skilaverkefni_3.py
+++ Skilaverkefni/Skilaverkefni_3/Skilaverkefni_3.py
@@ -8,7 +8,7 @@ def prentatuple(tup):
 
 
 tup_herrar = ("Steinn", "Kolbeinn", "Tryggvi", "Hugi", "Starri", "Bessi")
-tup_domur = ("Renata", "Thuridur", "Sigridur", "Maria", "Eilidh", "Mamma")
+tup_domur = ("Renata", "Thuridur", "Sigridur", "Maria", "Eilidh", "Jonina")
 prentatuple(tup_herrar)
 prentatuple(tup_domur)
 
@@ -32,28 +32,22 @@ def para_random(tup1, tup2):
 
 para_random(tup_domur, tup_herrar)
 
-def para_random_stakur(tup1,tup2):
+
+def para_random_stakur(tup1, tup2):
     print()
     herra_listi = []
     domu_listi = []
-    for i in range(20):
+    while len(herra_listi) != 6:
         random_herra = random.choice(tup1)
-        random_dama = random.choice(tup2)
-        # orugglega til audveldari leid en thetta
-        # nota thessa adferd til thess ad pass thad ad listin se ekki minni ne meiri en 6
-        if len(herra_listi) <= 5:
-            if random_herra not in herra_listi:
-                herra_listi.append(random_herra)
-        else:
-            pass
-        if len(domu_listi) <= 5:
-            if random_dama not in domu_listi:
-                domu_listi.append(random_dama)
-        else:
-            pass
+        if random_herra not in herra_listi:
+            herra_listi.append(random_herra)
 
-    for i in range(0, 6):
-        print(f"par {i}: {domu_listi[i]} og {herra_listi[i]}")
+    while len(domu_listi) != 6:
+        random_dama = random.choice(tup2)
+        if random_dama not in domu_listi:
+            domu_listi.append(random_dama)
+    for i in range(6):
+        print(f"Random par an duplicates {i + 1}: {domu_listi[i]} og {herra_listi[i]}")
 
 
 para_random_stakur(tup_herrar, tup_domur)
