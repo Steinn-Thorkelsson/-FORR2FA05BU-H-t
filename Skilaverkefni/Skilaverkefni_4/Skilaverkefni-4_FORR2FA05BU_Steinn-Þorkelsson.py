e3d61fa433177ae7dcd71cd9d84e064e99bbd0f0
Steinn-Thorkelsson <steinntho20@tskoli.is>
2020-11-03 10:45:15 +0000

    aaaaaaaaaaa

--- Skilaverkefni/Skilaverkefni_4/Skilaverkefni-4_FORR2FA05BU_Steinn-Þorkelsson.py
+++ Skilaverkefni/Skilaverkefni_4/Skilaverkefni-4_FORR2FA05BU_Steinn-Þorkelsson.py
@@ -2,6 +2,7 @@
 
 # Lidur 1 --------------------------------------------------------------------------------------------------------------
 # 1A
+
 def sletttolu_listi():
     tolulisti = []
     for t in range(2, 1001, 2):
@@ -78,7 +79,37 @@ def primtolu_listi():
     return prim_listi
 
 
-print(primtolu_listi())
+primtolur = primtolu_listi()
+skrifaiskra(primtolur, "primtolur.txt")
+
+lesa_primtolur = lesa_skra("primtolur.txt")
+prenta(lesa_primtolur)
+
+
+def inniheldur_7(listi):
+    nyr_listi = []
+    str_listi = [str(i) for i in listi]
+    for i in str_listi:
+        if '7' in i:
+            nyr_listi.append(i)
+    int_nyr_listi = [int(i) for i in nyr_listi]
+    print(int_nyr_listi)
+
+
+inniheldur_7(lesa_primtolur)
+
+
+def fjorda_hver_tala(listi):
+    fjorda_hver_listi = []
+    for i in range(3, len(listi), 3):
+        fjorda_hver_listi.append(listi[i])
+    return fjorda_hver_listi
+
+
+fjorda_hver_tala_listi = fjorda_hver_tala(lesa_primtolur)
+skrifaiskra(fjorda_hver_tala_listi, "fjorda.txt")
+
+print(fjorda_hver_tala(lesa_primtolur))
 
 on = True
 
@@ -94,7 +125,8 @@ while on:
     if val == "1":
         on1 = True
         while on1:
-            val1_listi = ['------ Liður 1 ------', '1. Prenta sléttölur upp í þúsund', "2. Prenta Meðaltal skránnar",
+            val1_listi = ['------ Liður 1 ------', '------Sléttar tölur ------', '1. Prenta sléttölur upp í þúsund',
+                          "2. Prenta Meðaltal skránnar",
                           '3. Búa til nýja lista með tölum sem 8 gengur upp í og prenta',
                           "4. Prenta fyrri listann með 10 tölum í hverri röð", "5 Hætta í Lið 1"]
             print()
@@ -136,11 +168,45 @@ while on:
 
             elif val1 == "5":
                 on1 = False
+
     elif val == "2":
-        pass
+
+        primtolur = primtolu_listi()
+        skrifaiskra(primtolur, "primtolur.txt")
+
+        on2 = True
+        while on2:
+            val2_listi = ["------ Liður 2 -----", "------ Prímtölur ------", "1. Búa til Prímtölu lista og prenta",
+                          "2. Finna allar prímtölur sem innihalda töluna 7 ", "3. Finna 4 hverja prímtölu", "4. Hætta"]
+            print()
+            for i in val2_listi:
+                print(i)
+            print()
+            val2 = input("Veldu fall: ")
+            print()
+
+            if val2 == "1":
+                print()
+                lesa_primtolur = lesa_skra("primtolur.txt")
+                prenta(lesa_primtolur)
+
+            elif val2 == "2":
+                print()
+                inniheldur_7(lesa_primtolur)
+
+            elif val2 == "3":
+                fjorda_hver_tala_listi = fjorda_hver_tala(lesa_primtolur)
+                skrifaiskra(fjorda_hver_tala_listi, "fjorda.txt")
+
+                print(fjorda_hver_tala(lesa_primtolur))
+            elif val2 == "4":
+                on2 = False
+
     elif val == "3":
         pass
+
     elif val == "4":
         pass
+
     elif val == "5":
         break
